228337731654f8a9fc060a1f65c5a3da3cdd12ca
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2025-11-19 01:19:49 +0530

    Delete frontend/src directory

--- frontend/src/api/addressApi.js
+++
@@ -1,9 +0,0 @@
-// src/api/addressApi.js
-import axiosInstance from "./axiosInstance";
-
-export const getAddresses = () => axiosInstance.get("/addresses");
-
-export const addAddress = (addressData) =>
-  axiosInstance.post("/addresses", addressData);
-
-export const deleteAddress = (id) => axiosInstance.delete(`/addresses/${id}`);
