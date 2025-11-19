4411a5205dc60945c60a6f0b195bed88b42bfb1b
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2025-11-19 23:00:18 +0530

    Delete frontend/src directory

--- frontend/src/api/reviewApi.js
+++
@@ -1,8 +0,0 @@
-// src/api/reviewApi.js
-import axiosInstance from "./axiosInstance";
-
-export const addReview = (product_id, rating, comment) =>
-  axiosInstance.post("/reviews", { product_id, rating, comment });
-
-export const getReviewsForProduct = (product_id) =>
-  axiosInstance.get(`/reviews/product/${product_id}`);
