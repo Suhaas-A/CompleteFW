4a6368008306194598acfe4171b77e84e9bc4c45
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2025-12-26 17:09:54 +0530

    Update AdminLayout.jsx

--- frontend/src/components/admin/AdminLayout.jsx
+++ frontend/src/components/admin/AdminLayout.jsx
@@ -4,10 +4,10 @@ import AdminSidebar from "../../components/admin/AdminSidebar";
 export default function AdminLayout({ children }) {
   return (
     <div className="min-h-screen bg-[#F9F9F7] flex flex-col md:flex-row">
-      <AdminSidebar />
       <main className="flex-1 w-full md:p-8 p-4 pt-[120px] md:pt-8 overflow-x-hidden">
         {children}
       </main>
     </div>
   );
 }
+
