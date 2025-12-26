c9d3033169cab14352b4bb0c680a6e8d0e443327
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2025-12-26 14:51:40 +0530

    Update AppProviders.jsx

--- frontend/src/AppProviders.jsx
+++ frontend/src/AppProviders.jsx
@@ -1,14 +1,18 @@
-// src/AppProviders.jsx
 import { AuthProvider } from "./contexts/AuthContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
 import { CartProvider } from "./contexts/CartContext";
 import { WishlistProvider } from "./contexts/Wishlist";
 
 export default function AppProviders({ children }) {
   return (
     <AuthProvider>
-      <CartProvider>
-        <WishlistProvider>{children}</WishlistProvider>
-      </CartProvider>
+      <ThemeProvider>
+        <CartProvider>
+          <WishlistProvider>
+            {children}
+          </WishlistProvider>
+        </CartProvider>
+      </ThemeProvider>
     </AuthProvider>
   );
 }
