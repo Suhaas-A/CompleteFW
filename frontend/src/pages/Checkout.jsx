79115304b3bfdc9572d4e0c464676c08588981cb
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2026-01-10 22:26:43 +0530

    Update Checkout.jsx

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -170,6 +170,7 @@ export default function Checkout() {
         products,
         delivery_link: paymentMethod === "online" ? "pending" : "cod",
         status: paymentMethod === "online" ? "Payment Pending" : "Pending",
+        total_amount=order.total_amount,
       });
 
       const orderId = orderRes.data.order_id;
@@ -391,6 +392,7 @@ export default function Checkout() {
     </div>
   );
 }
+
 
 
 
